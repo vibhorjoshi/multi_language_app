2496937f000d83387d03abc80d5136248e171c02
vibhorjoshi <105739194+vibhorjoshi@users.noreply.github.com>
2024-06-03 20:31:38 +0700

    Update speech_recognition.py

--- speech_recognition.py
+++ speech_recognition.py
@@ -1,7 +1,19 @@
-# speech_recognition.py
 import speech_recognition as sr
+from pydub import AudioSegment
+import os
 
-def recognize_speech(audio_file):
+def extract_audio_from_video(video_file, audio_file):
+    video = AudioSegment.from_file(video_file, format="mp4")
+    video.export(audio_file, format="wav")
+
+def recognize_speech(file_path):
+    # Check if the file is a video
+    if file_path.lower().endswith(".mp4"):
+        audio_file = file_path.replace(".mp4", ".wav")
+        extract_audio_from_video(file_path, audio_file)
+    else:
+        audio_file = file_path
+    
     recognizer = sr.Recognizer()
     with sr.AudioFile(audio_file) as source:
         audio = recognizer.record(source)
@@ -14,6 +26,6 @@ def recognize_speech(audio_file):
         return f"Could not request results from Google Speech Recognition service; {e}"
 
 if __name__ == "__main__":
-    audio_file = "path/to/audio/file.wav"  # Provide the path to your audio file
-    text = recognize_speech(audio_file)
+    file_path = "audio/20230103_013922.mp4"  # Provide the path to your audio or video file
+    text = recognize_speech(file_path)
     print(f"Recognized text: {text}")
